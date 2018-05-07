d553b14deff4a82f1d063f5bf73f9479d68bf2b4
Mubuntu <patrick.a.frison@gmail.com>
2018-05-07 12:40:27 +0200

    initial commit

---
+++ site/src/main/java/be/cornetto/beans/BaseDocument.java
@@ -0,0 +1,9 @@
+package be.cornetto.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+
+@Node(jcrType="hippoproject:basedocument")
+public class BaseDocument extends HippoDocument {
+
+}
